0d9f839a5d431fe847f8366898a101341a8c5ef8
Karthikmnagaraja <karthikmnagaraja@gmail.com>
2015-04-30 12:45:22 -0400

    Adding angular dependencies

--- server.js
+++ server.js
@@ -7,7 +7,13 @@
 var express    = require('express');        // call express
 var app        = express();                 // define our app using express
 var bodyParser = require('body-parser');
-
+var path = require('path');
+var favicon = require('serve-favicon');
+var logger = require('morgan');
+var cookieParser = require('cookie-parser');
+var bodyParser = require('body-parser');
+var routes = require('./routes/index');
+var users = require('./routes/users');
 //OAuth
 var method = 'POST';
 var host = 'api.us1.covisint.com';
@@ -22,7 +28,20 @@ type = 'client_credentials';
 applicationid = 'mk7SgGMf4alufI9zOoeZ7jpZrfb5dOjU';
 }
 
+//view engine setup
+app.set('views', path.join(__dirname, 'views'));
+app.set('view engine', 'jade');
 
+    // uncomment after placing your favicon in /public
+    //app.use(favicon(__dirname + '/public/favicon.ico'));
+            app.use(logger('dev'));
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(cookieParser());
+app.use(express.static(path.join(__dirname, 'public')));
+
+    app.use('/', routes);
+app.use('/users', users);
 var username = '1ed80977-1650-4403-8679-080e8b840dfa';
 var password = 'e4156c68-1d24-4d54-a604-6e872f63eb94';
 
