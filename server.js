fc2d1de2af684c13d80e0658243436b7250ede51
srinathkotu <1.fr.204@gmail.com>
2015-04-30 13:27:34 -0400

    removed old server.js code

--- server.js
+++ server.js
@@ -14,19 +14,6 @@ var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
 var routes = require('./routes/index');
 var users = require('./routes/users');
-//OAuth
-var method = 'POST';
-var host = 'api.us1.covisint.com';
-var https = require('https');
-var authorization = 'Basic MWVkODA5NzctMTY1MC00NDAzLTg2NzktMDgwZThiODQwZGZhOmU0MTU2YzY4LTFkMjQtNGQ1NC1hNjA0LTZlODcyZjYzZWI5NA==';
-var accept = '';
-var type = '';
-var applicationid = '';
-function OAuth(){
-accept = 'application/vnd.com.covisint.platform.oauth.token.v1+json';
-type = 'client_credentials';
-applicationid = 'mk7SgGMf4alufI9zOoeZ7jpZrfb5dOjU';
-}
 
 //view engine setup
 app.set('views', path.join(__dirname, 'views'));
@@ -42,65 +29,10 @@ app.use(express.static(path.join(__dirname, 'public')));
 
     app.use('/', routes);
 app.use('/users', users);
-var username = '1ed80977-1650-4403-8679-080e8b840dfa';
-var password = 'e4156c68-1d24-4d54-a604-6e872f63eb94';
 
 
 
 
-function performRequest(endpoint, method, data, success) {
-var dataString = JSON.stringify(data);
-var headers = {};
-
-if (method == 'GET') {
-    endpoint += '?' + querystring.stringify(data);
-  }
-  else {
-    headers = {
-      'Authorization' : authorization,
-      'Accept' : accept,
-      'Type' : type,
-      'application-id' : applicationid
-    };
-  }
-  var options = {
-    host: host,
-    path: endpoint,
-    method: method,
-    headers: headers
-  };
-
-  var req = https.request(options, function(res) {
-    res.setEncoding('utf-8');
-
-    var responseString = '';
-
-    res.on('data', function(data) {
-      responseString += data;
-    });
-
-    res.on('end', function() {
-      //console.log(responseString);
-      var responseObject = JSON.parse(responseString);
-      success(responseObject);
-    });
-  });
-
-  req.write(dataString);
-  req.end();
-}
-
-function login() {
-OAuth();
-  performRequest('/oauth/v1/token', 'POST', {
-    username: username,
-    password: password
-  }, function(data) {
-    //sessionId = data.result.id;
-    console.log('Logged in:', data.creator);
-//    getCards();
-  });
-}
 
 
 ////////////////////////////////////////////////////////////////////////////////////
@@ -132,4 +64,3 @@ login();
 // START THE SERVER
 // =============================================================================
 app.listen(port);
-console.log('Magic happens on port ' + port);
