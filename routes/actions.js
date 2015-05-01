98096e9a6d8845a3aa9a8d1ec722c75535865999
unknown <dleinbach@DTW125315N01.covisint.corp>
2015-05-01 02:49:16 -0400

    remove connect and end for mysql

--- routes/actions.js
+++ routes/actions.js
@@ -4,9 +4,9 @@ var router = express.Router();
 var host = 'api.us1.covisint.com';
 
 var connection = mysql.createConnection({
-  host     : '10.96.160.202',
-  // host: 'localhost',
-  // port: '13306',
+  // host     : '10.96.160.202',
+  host: 'localhost',
+  port: '13306',
   user     : 'puNMbiYPZdMTyH5J',
   password : 'GYUWpW9p7wGnWdsc',
   database : 'cf_7d851986_532a_479a_93d3_e5ed66102f4c'
@@ -20,8 +20,6 @@ router.get('/arduino', function(req, res, next) {
     res.removeHeader('Date');
     res.removeHeader('ETag');
 
-    connection.connect();
-
 		connection.query('select * from actions where deviceId = \''+req.query.deviceId+'\' AND ack = 0 limit 1;', function(err, rows, fields) {
 		  if (err){
 		  	console.log("Got error: ");
@@ -32,15 +30,13 @@ router.get('/arduino', function(req, res, next) {
 
 		  	if(rows.length > 0){
 			  	console.log('The solution is: ', rows[0]);
-			  	response.header('pin',rows[0].pin);
-			  	response.header('val',rows[0].val);
+			  	res.header('pin',rows[0].pin);
+			  	res.header('val',rows[0].val);
 				}
 			  res.status(200);
 			  res.end();
 			}
 		});
-
-		connection.end();
 
 });
 
